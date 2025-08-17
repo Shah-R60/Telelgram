0a1375319a9d6a5f88edb3560e11c2f5699adc99
Shah-R60 <shahrahul3600@gmail.com>
2025-08-17 22:52:35 +0530

    feat: implement channel creation on user press in UserListItem component

--- src/components/UserLIstItem.tsx
+++ src/components/UserLIstItem.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text , Pressable } from 'react-native'
 import React from 'react'
+import { useChatContext } from 'stream-chat-expo';
+import { useAuth } from '../app/providers/AuthProvider';
+import { router } from 'expo-router';
 
 const UserListItem = ({ user }) => {
+    const  {client} = useChatContext();
+    const {user:me} = useAuth();
+    const onPress = async()=>{
+        const channel = client.channel('messaging', {
+            members: [me.id, user.id]
+        });
+        channel.watch();
+
+        try{
+            await channel.watch();
+            router.replace(`/(home)/channel/${channel.cid}`);
+        }catch(error){
+            console.error('Error creating channel:', error);
+        }
+    }
   return (
-    <View style={{ padding: 12, borderBottomWidth: 1, backgroundColor: 'white', borderBottomColor: '#ff0f0fff' }}>
-      <Text style={{ fontWeight: 'bold' }}>{user.full_name}</Text>
-    </View>
+    <Pressable onPress={onPress}>
+      <View style={{ padding: 12, borderBottomWidth: 1, backgroundColor: 'white', borderBottomColor: '#ff0f0fff' }}>
+        <Text style={{ fontWeight: 'bold' }}>{user.full_name}</Text>
+      </View>
+    </Pressable>
   )
 }
 
