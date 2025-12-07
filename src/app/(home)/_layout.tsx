385e14c8508e64206aff6e865a463ac0c1fa404c
Shah-R60 <shahrahul3600@gmail.com>
2025-12-07 12:25:48 +0530

    feat: update HomeLayout and HomeIndex to include additional Stack.Screen components for improved navigation

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -18,7 +18,11 @@ export default function HomeLayout() {
         <VideoProvider>
           <CallProvider>
             <Stack>
+              <Stack.Screen name="index" options={{ headerShown: false }} />
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="users" options={{ title: 'Users' }} />
+              <Stack.Screen name="channel/[cid]" options={{ title: 'Channel' }} />
+              <Stack.Screen name="call/index" options={{ headerShown: false, title: 'Call' }} />
             </Stack>
           </CallProvider>
         </VideoProvider>
--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -1,12 +1,5 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { Redirect } from 'expo-router';
 
-const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  );
-};
-
-export default index;
+export default function HomeIndex() {
+  return <Redirect href="/(home)/(tabs)" />;
+}
